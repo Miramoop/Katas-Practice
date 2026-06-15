3521fd425660927fd451d1b8121bd0f67d3a465a
Miramoop <94880693+Miramoop@users.noreply.github.com>
2026-06-15 13:31:38 -0400

    Add Tire Pressure Kata

---
+++ Examples/TirePressure-csharp.xUnit/TirePressure.Tests/AlarmTest.cs
@@ -0,0 +1,14 @@
+﻿using TirePressure_csharp.xUnit;
+
+namespace TirePressure.Tests
+{
+    public class AlarmTest
+    {
+        [Fact]
+        public void AlarmIsOffByDefault()
+        {
+            Alarm alarm = new Alarm();
+            Assert.False(alarm.AlarmOn);
+        }
+    }
+}
