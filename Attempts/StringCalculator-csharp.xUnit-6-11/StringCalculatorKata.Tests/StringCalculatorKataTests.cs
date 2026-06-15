dee164c979a5673de447ae22d23bfab8eb7c2170
Miranda Morris <juniperistic@gmail.com>
2026-06-14 21:29:24 -0400

    Adding 6-11 Attempt

---
+++ Attempts/StringCalculator-csharp.xUnit-6-11/StringCalculatorKata.Tests/StringCalculatorKataTests.cs
@@ -0,0 +1,66 @@
+﻿using StringCalculatorAttempt6_11;
+
+namespace StringCalculatorKata.Tests;
+
+public class StringCalculatorKataTests
+{
+    [Fact]
+    public void Add_GivenEmptyString_Return0()
+    {
+        var calculator = new StringCalculator();
+        var result = calculator.Add("");
+        Assert.Equal(0, result);
+    }
+    [Fact]
+    public void Add_GivenNumber_ReturnNumber()
+    {
+        var calculator = new StringCalculator();
+        var result = calculator.Add("1");
+        Assert.Equal(1, result);
+    }
+    [Fact]
+    public void Add_GivenTwoNumbers_ReturnsSum()
+    {
+        var calculator = new StringCalculator();
+        var result = calculator.Add("1,2");
+        Assert.Equal(3, result);
+    }
+    [Fact]
+    public void Add_GivenMultipleNumbers_ReturnsSum()
+    {
+        var calculator = new StringCalculator();
+        var result = calculator.Add("1,2,3,0");
+        Assert.Equal(6, result);
+    }
+    [Fact]
+    public void Add_GivenNegativeNumbers_ThrowNotAllowedError()
+    {
+        var calculator = new StringCalculator();
+        var response = Assert.Throws<Exception>(() => calculator.Add("-1,-4,0"));
+        Assert.Equal("Negative Numbers Not Allowed: -1,-4", response.Message);
+    }
+
+    [Fact]
+    public void Add_GivenStringEndingInComma_ThrowEndOfFileFoundError()
+    {
+        var calculator = new StringCalculator();
+        var response = Assert.Throws<Exception>(() => calculator.Add("1,2,3,"));
+        Assert.Equal("Number expected but EOF found.",response.Message);
+    }
+    
+    [Fact]
+    public void Add_GivenStringEndingInNewLine_ThrowEndOfFileFoundError()
+    {
+        var calculator = new StringCalculator();
+        var response = Assert.Throws<Exception>(() => calculator.Add("1,2,3\n"));
+        Assert.Equal("Number expected but EOF found.",response.Message);
+    }
+
+    [Fact]
+    public void Add_GivenStringWithNewLineSeparator_ReturnSum()
+    {
+        var calculator = new StringCalculator();
+        var result = calculator.Add("1\n2");
+        Assert.Equal(3,result);
+    }
+}
---
+++ Attempts/StringCalculator-csharp.xUnit-6-11/StringCalculatorKata/StringCalculator.cs
@@ -0,0 +1,46 @@
+﻿namespace StringCalculatorAttempt6_11;
+
+public class StringCalculator
+{
+    public int Add(string numbers)
+    {
+        if (string.IsNullOrEmpty(numbers))
+        {
+            return 0;
+        }
+
+        if (numbers.EndsWith(',') || numbers.EndsWith('\n'))
+        {
+            throw new Exception("Number expected but EOF found.");
+        }
+        
+        if (numbers.Contains('-'))
+        {
+            var listOfNegativeNumbers = numbers.Split(',')
+                .Select(int.Parse)
+                .Where(x => x < 0);
+            var delimiter = ",";
+            var stringOfNegativeNumbers = string.Join(delimiter, listOfNegativeNumbers);
+        
+            throw new Exception($"Negative Numbers Not Allowed: {stringOfNegativeNumbers}");
+        }
+
+        if (numbers.Contains(','))
+        {
+            var total = numbers.Split(',')
+                .Select(int.Parse)
+                .Sum();
+            return total;
+        }
+
+        if (numbers.Contains('\n'))
+        {
+            var total = numbers.Split('\n')
+                .Select(int.Parse)
+                .Sum();
+            return total;
+        }
+        
+        return (int.Parse(numbers));
+    }
+}
