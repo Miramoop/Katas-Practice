8fb54d4b16cd79f735b0306ce5602acdcad30048
Miramoop <94880693+Miramoop@users.noreply.github.com>
2026-06-15 11:45:54 -0400

    Finished 100 Doors Kata Attempt from 6-12

---
+++ Attempts/100DoorsKata-csharp-xUnit-6-12/100DoorsKataBase/100DoorsKata.cs
@@ -0,0 +1,22 @@
+﻿namespace _100DoorsKataBase;
+
+public class _100Doors
+{
+    public List<bool> Doors { get; set; } = new List<bool>(new bool[100]);
+
+    public void ToggleAllDoors()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Doors[i] = true;
+        }
+    }
+    
+    public void ToggleAllDoors(int pass)
+    {
+        for (int i = 0; i < 100; i += pass)
+        {
+            Doors[i] = false;
+        }
+    }
+}
---
+++ Attempts/100DoorsKata-csharp-xUnit-6-12/100DoorsKataTests/100DoorsKata.Tests.cs
@@ -0,0 +1,48 @@
+﻿using _100DoorsKataBase;
+
+namespace _100DoorsKataTests;
+
+public class _100DoorsKataTests
+{
+    [Fact]
+    public void Doors_CheckFor100DoorsInList()
+    {
+        var doors = new _100Doors();
+        Assert.Equal(100, doors.Doors.Count);
+    }
+
+    [Fact]
+    public void Doors_CheckAllDoorsFalse()
+    {
+        var doors = new _100Doors();
+        Assert.Equal(true, doors.Doors.All(x => x == false));
+    }
+
+    [Fact]
+    public void Doors_ToggleAllDoors()
+    {
+        var doors = new _100Doors();
+        Assert.Equal(true, doors.Doors.All(x => x == false));
+
+        doors.ToggleAllDoors();
+        Assert.Equal(true, doors.Doors.All(x=> x == true));
+    }
+
+    [Fact]
+    public void Doors_GivenPass2_EverySecondDoorShouldBeClosed()
+    {
+        var doors = new _100Doors();
+        doors.ToggleAllDoors(2);
+        Assert.Equal(false, doors.Doors[1]);
+        Assert.Equal(false, doors.Doors[3]);
+    }
+
+    [Fact]
+    public void Doors_GivenPass3_EveryThirdDoorShouldBeToggled()
+    {
+        var doors = new _100Doors();
+        doors.ToggleAllDoors(3);
+        Assert.Equal(false, doors.Doors[2]);
+        Assert.Equal(false, doors.Doors[5]);
+    }
+}
