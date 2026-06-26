00fdeb8f311743e17fc53299d762dbdcb77e1af0
Miramoop <94880693+Miramoop@users.noreply.github.com>
2026-06-26 16:53:47 -0400

    Upload Vending Machine Kata from today

---
+++ Attempts/VendingMachine-csharp-xUnit-6-26/VendingMachineKata/VendingMachineKata.Tests/VendingMachineTests.cs
@@ -0,0 +1,121 @@
+﻿namespace VendingMachineKata.Tests;
+
+public class VendingMachineTests
+{
+    [Fact]
+    public void SelectItem_GivenA01_ReturnSoda()
+    {
+        var vendingMachine = new VendingMachine();
+        var result = vendingMachine.SelectItem("A01");
+        Assert.Equal("Soda", result);
+    }
+    
+    [Fact]
+    public void SelectItem_GivenA02_ReturnChips()
+    {
+        var vendingMachine = new VendingMachine();
+        var result = vendingMachine.SelectItem("A02");
+        Assert.Equal("Chips", result);
+    }
+    
+    [Fact]
+    public void SelectItem_GivenA03_ReturnCandy()
+    {
+        var vendingMachine = new VendingMachine();
+        var result = vendingMachine.SelectItem("A03");
+        Assert.Equal("Candy", result);
+    }
+    
+    [Fact]
+    public void SelectItem_GivenA04_ReturnGum()
+    {
+        var vendingMachine = new VendingMachine();
+        var result = vendingMachine.SelectItem("A04");
+        Assert.Equal("Gum", result);
+    }
+    
+    [Fact]
+    public void SelectItem_GivenInvalidCode_ReturnError()
+    {
+        var vendingMachine = new VendingMachine();
+        var result = vendingMachine.SelectItem("AAA");
+        Assert.Equal("Invalid Code Entered", result);
+    }
+    
+    [Fact]
+    public void InputChange_GivenOneQuarter_Return25()
+    {
+        var vendingMachine = new VendingMachine();
+        var result = vendingMachine.InputChange(25);
+        Assert.Equal("25", result);
+    }
+    
+    [Fact]
+    public void InputChange_GivenTwoQuarters_Return50()
+    {
+        var vendingMachine = new VendingMachine();
+        var result = vendingMachine.InputChange(25);
+        vendingMachine.InputChange(25);
+        Assert.Equal("50", vendingMachine.changeEntered.ToString());
+    }
+    
+    [Fact]
+    public void CompletePurchase_GivenA01After4Quarters_ReturnVendingSoda()
+    {
+        var vendingMachine = new VendingMachine();
+        vendingMachine.SelectItem("A01");
+        var result = vendingMachine.InputChange(25);
+        vendingMachine.InputChange(25);
+        vendingMachine.InputChange(25);
+        vendingMachine.InputChange(25);
+        Assert.Equal("Vending Soda", vendingMachine.output);
+    }
+    
+    [Fact]
+    public void CompletePurchase_GivenA02AfterCorrectChange_ReturnVendingChips()
+    {
+        var vendingMachine = new VendingMachine();
+        vendingMachine.SelectItem("A02");
+        var result = vendingMachine.InputChange(25);
+        vendingMachine.InputChange(25);
+        vendingMachine.InputChange(10);
+        vendingMachine.InputChange(5);
+        Assert.Equal("Vending Chips", vendingMachine.output);
+    }
+    
+    [Fact]
+    public void CompletePurchase_GivenA01AfterCorrectChange_ReturnVendingCandy()
+    {
+        var vendingMachine = new VendingMachine();
+        vendingMachine.SelectItem("A03");
+        var result = vendingMachine.InputChange(25);
+        vendingMachine.InputChange(25);
+        vendingMachine.InputChange(25);
+        vendingMachine.InputChange(10);
+        Assert.Equal("Vending Candy", vendingMachine.output);
+    }
+    
+    [Fact]
+    public void CompletePurchase_GivenA04AfterCorrectChange_ReturnVendingGum()
+    {
+        var vendingMachine = new VendingMachine();
+        vendingMachine.SelectItem("A04");
+        var result = vendingMachine.InputChange(25);
+        vendingMachine.InputChange(10);
+        vendingMachine.InputChange(5);
+        Assert.Equal("Vending Gum", vendingMachine.output);
+    }
+    
+    [Fact]
+    public void CompletePurchase_GivenA01AfterExtraChange_ReturnSodaAndExtraChange()
+    {
+        var vendingMachine = new VendingMachine();
+        vendingMachine.SelectItem("A01");
+        var result = vendingMachine.InputChange(25);
+        vendingMachine.InputChange(25);
+        vendingMachine.InputChange(25);
+        vendingMachine.InputChange(25);
+        vendingMachine.InputChange(25);
+        Assert.Equal("Vending Soda: Change $0.25", vendingMachine.output);
+    }
+}
---
+++ Attempts/VendingMachine-csharp-xUnit-6-26/VendingMachineKata/VendingMachineKata/VendingMachine.cs
@@ -0,0 +1,74 @@
+﻿namespace VendingMachineKata;
+
+public class VendingMachine
+{
+    public int changeEntered { get; set; }
+    public string selectedItem { get; set; }
+    public string output { get; set; }
+    public int returnedChange { get; set; }
+    
+    public string SelectItem(string code)
+    {
+        if (code == "A01")
+        {
+            selectedItem = "Soda";
+            return "Soda";
+        }
+        
+        if (code == "A02")
+        {
+            selectedItem = "Chips";
+            return "Chips";
+        }
+        
+        if (code == "A03")
+        {
+            selectedItem = "Candy";
+            return "Candy";
+        }
+        
+        if (code == "A04")
+        {
+            selectedItem = "Gum";
+            return "Gum";
+        }
+
+        return "Invalid Code Entered";
+    }
+
+    public string InputChange(int change)
+    {
+        changeEntered += change;
+
+        if (selectedItem == "Gum" && changeEntered >= 40)
+        {
+            output = "Vending Gum";
+            returnedChange = changeEntered - 40;
+        }
+        
+        if (selectedItem == "Candy" && changeEntered >= 85)
+        {
+            output = "Vending Candy";
+            returnedChange = changeEntered - 85;
+        }
+        
+        if (selectedItem == "Chips" && changeEntered >= 65)
+        {
+            output = "Vending Chips";
+            returnedChange = changeEntered - 65;
+        }
+        
+        if (selectedItem == "Soda" && changeEntered >= 100)
+        {
+            output = "Vending Soda";
+            returnedChange = changeEntered - 100;
+            if (changeEntered > 100)
+            {
+                output = "Vending Soda: Change $0.25";
+            }
+        }
+        
+        return changeEntered.ToString();
+        
+    }
+}
