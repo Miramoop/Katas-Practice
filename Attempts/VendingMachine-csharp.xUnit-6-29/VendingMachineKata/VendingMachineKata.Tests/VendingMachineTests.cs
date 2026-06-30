9c4d74098fa4c024ddcefd822180a0f47d24e351
Miramoop <94880693+Miramoop@users.noreply.github.com>
2026-06-29 21:13:49 -0400

    Add 6-29 Attempt at Vending Machine Kata

---
+++ Attempts/VendingMachine-csharp.xUnit-6-29/VendingMachineKata/VendingMachineKata.Tests/VendingMachineTests.cs
@@ -0,0 +1,173 @@
+﻿namespace VendingMachineKata.Tests;
+
+public class VendingMachineTests
+{
+    [Fact]
+    public void Vend_GivenSodaCode_ReturnSoda()
+    {
+        var vendingMachine = new VendingMachine();
+        var result = vendingMachine.Vend("A01");
+        Assert.Equal("Soda", result);
+    }
+    
+    [Fact]
+    public void Vend_GivenChipsCode_ReturnChips()
+    {
+        var vendingMachine = new VendingMachine();
+        var result = vendingMachine.Vend("A02");
+        Assert.Equal("Chips", result);
+    }
+    
+    [Fact]
+    public void Vend_GivenCandyCode_ReturnCandy()
+    {
+        var vendingMachine = new VendingMachine();
+        var result = vendingMachine.Vend("A03");
+        Assert.Equal("Candy", result);
+    }
+    
+    [Fact]
+    public void Vend_GivenGumCode_ReturnGum()
+    {
+        var vendingMachine = new VendingMachine();
+        var result = vendingMachine.Vend("A04");
+        Assert.Equal("Gum", result);
+    }
+    
+    [Fact]
+    public void Vend_GivenInvalidCode_ReturnInvalidCodeMessage()
+    {
+        var vendingMachine = new VendingMachine();
+        var result = vendingMachine.Vend("A08");
+        Assert.Equal("Invalid Selection: A08", result);
+    }
+    
+    [Fact]
+    public void InputChange_GivenOneQuarter_ReturnTwentyFive()
+    {
+        var vendingMachine = new VendingMachine();
+        vendingMachine.InputChange(25);
+        Assert.Equal(25, vendingMachine.ChangeEntered);
+    }
+    
+    [Fact]
+    public void InputChange_GivenTwoQuarters_ReturnFifty()
+    {
+        var vendingMachine = new VendingMachine();
+        vendingMachine.InputChange(25);
+        vendingMachine.InputChange(25);
+        Assert.Equal(50, vendingMachine.ChangeEntered);
+    }
+    
+    [Fact]
+    public void InputChange_GivenFourQuarters_ReturnSoda()
+    {
+        var vendingMachine = new VendingMachine();
+        vendingMachine.InputChange(25);
+        vendingMachine.InputChange(25);
+        vendingMachine.InputChange(25);
+        vendingMachine.InputChange(25);
+        vendingMachine.Vend("A01");
+        Assert.Equal("Vending Soda", vendingMachine.Output);
+    }
+    
+    [Fact]
+    public void InputChange_GivenCorrectChange_ReturnChips()
+    {
+        var vendingMachine = new VendingMachine();
+        vendingMachine.InputChange(25);
+        vendingMachine.InputChange(25);
+        vendingMachine.InputChange(10);
+        vendingMachine.InputChange(5);
+        vendingMachine.Vend("A02");
+        Assert.Equal("Vending Chips", vendingMachine.Output);
+    }
+    
+    [Fact]
+    public void InputChange_GivenCorrectChange_ReturnCandy()
+    {
+        var vendingMachine = new VendingMachine();
+        vendingMachine.InputChange(25);
+        vendingMachine.InputChange(25);
+        vendingMachine.InputChange(25);
+        vendingMachine.InputChange(10);
+        vendingMachine.Vend("A03");
+        Assert.Equal("Vending Candy", vendingMachine.Output);
+    }
+    
+    [Fact]
+    public void InputChange_GivenCorrectChange_ReturnGum()
+    {
+        var vendingMachine = new VendingMachine();
+        vendingMachine.InputChange(25);
+        vendingMachine.InputChange(10);
+        vendingMachine.InputChange(5);
+        vendingMachine.Vend("A04");
+        Assert.Equal("Vending Gum", vendingMachine.Output);
+    }
+    
+    [Fact]
+    public void InputChange_GivenExtraChange_ReturnGumAndChange()
+    {
+        var vendingMachine = new VendingMachine();
+        vendingMachine.InputChange(25);
+        vendingMachine.InputChange(25);
+        vendingMachine.InputChange(25);
+        vendingMachine.InputChange(25);
+        vendingMachine.Vend("A04");
+        Assert.Equal((decimal)0.60, vendingMachine.ExtraChange);
+        Assert.Equal("Vending Gum: Change $0.60", vendingMachine.Output);
+    }
+    
+    [Fact]
+    public void InputChange_GivenInsufficientChange_ReturnFeedMeMessage()
+    {
+        var vendingMachine = new VendingMachine();
+        vendingMachine.InputChange(25);
+        vendingMachine.Vend("A04");
+        Assert.Equal((decimal)0.15, vendingMachine.RequiredChange);
+        Assert.Equal("Feed me $0.15 more", vendingMachine.Output);
+    }
+    
+    [Fact]
+    public void VendingMachine_GivenGum_ReturnGumQuantity()
+    {
+        var vendingMachine = new VendingMachine();
+        var item = vendingMachine.items.FirstOrDefault(x => x.Code == "A04");
+        Assert.Equal(3, item.Quantity);
+    }
+    
+    [Fact]
+    public void VendingMachine_GivenGum_ReturnGumQuantityMinusOne()
+    {
+        var vendingMachine = new VendingMachine();
+        var item = vendingMachine.items.FirstOrDefault(x => x.Code == "A04");
+        vendingMachine.InputChange(25);
+        vendingMachine.InputChange(10);
+        vendingMachine.InputChange(5);
+        vendingMachine.Vend("A04");
+        Assert.Equal(2, item.Quantity);
+    }
+    
+    [Fact]
+    public void VendingMachine_GivenGum_ReturnSoldOutMessage()
+    {
+        var vendingMachine = new VendingMachine();
+        var item = vendingMachine.items.FirstOrDefault(x => x.Code == "A04");
+        vendingMachine.InputChange(25);
+        vendingMachine.InputChange(10);
+        vendingMachine.InputChange(5);
+        vendingMachine.Vend("A04");
+        vendingMachine.InputChange(25);
+        vendingMachine.InputChange(10);
+        vendingMachine.InputChange(5);
+        vendingMachine.Vend("A04");
+        vendingMachine.InputChange(25);
+        vendingMachine.InputChange(10);
+        vendingMachine.InputChange(5);
+        vendingMachine.Vend("A04");
+        vendingMachine.Vend("A04");
+        Assert.Equal(0, item.Quantity);
+        Assert.Equal($"Sold Out: Gum",vendingMachine.Output);
+    }
+}
---
+++ Attempts/VendingMachine-csharp.xUnit-6-29/VendingMachineKata/VendingMachineKata/VendingMachineKata.cs
@@ -0,0 +1,113 @@
+﻿namespace VendingMachineKata;
+
+public class Item
+{
+    public string ItemName { get; set; }
+    public decimal Price { get; set; }
+    public string Code { get; set; }
+    public int Quantity { get; set; }
+}
+public class VendingMachine
+{
+    public string Output { get; set; }
+    public decimal ChangeEntered { get; set; }
+    public decimal ExtraChange { get; set; }
+    public decimal RequiredChange { get; set; }
+    public List<Item> items { get; set; } = new List<Item>();
+
+    public VendingMachine()
+    {
+        var Soda = new Item();
+        Soda.ItemName = "Soda";
+        Soda.Price = (decimal)1.00;
+        Soda.Code = "A01";
+        Soda.Quantity = 10;
+        items.Add(Soda);
+
+        var Chips = new Item();
+        Chips.ItemName = "Chips";
+        Chips.Price = (decimal)0.65;
+        Chips.Code = "A02";
+        Chips.Quantity = 12;
+        items.Add(Chips);
+
+        var Candy = new Item();
+        Candy.ItemName = "Candy";
+        Candy.Price = (decimal)0.85;
+        Candy.Code = "A03";
+        Candy.Quantity = 8;
+        items.Add(Candy);
+            
+        var Gum = new Item();
+        Gum.ItemName = "Gum";
+        Gum.Price = (decimal)0.40;
+        Gum.Code = "A04";
+        Gum.Quantity = 3;
+        items.Add(Gum);
+    }
+    public string Vend(string code)
+    {
+        if (code == "A01")
+        {
+            if (ChangeEntered >= 100)
+            {
+                Output = "Vending Soda";
+            }
+            return "Soda";
+        }
+
+        if (code == "A02")
+        {
+            if (ChangeEntered >= 65)
+            {
+                Output = "Vending Chips";
+            }
+            return "Chips";
+        }
+
+        if (code == "A03")
+        {
+            if (ChangeEntered >= 85)
+            {
+                Output = "Vending Candy";
+            }
+            return "Candy";
+        }
+
+        if (code == "A04")
+        {
+            var item = items.FirstOrDefault(x => x.Code == "A04");
+            
+            if (ChangeEntered >= 40 && item.Quantity != 0)
+            {
+
+                Output = "Vending Gum";
+                ExtraChange = (ChangeEntered - 40) / 100;
+                if (ExtraChange > 0)
+                {
+                    Output = $"Vending Gum: Change {ExtraChange:C2}";
+                }
+
+                //var item = items.FirstOrDefault(x => x.Code == "A04");
+                item.Quantity--;
+            }
+            else if (item.Quantity == 0)
+            {
+                Output = $"Sold Out: Gum";
+            }
+            else 
+            {
+                RequiredChange = (40 - ChangeEntered) / 100;
+                Output = $"Feed me {RequiredChange:C2} more";
+            } 
+            return "Gum"; 
+        }
+        
+        return $"Invalid Selection: {code}";
+    }
+
+    public void InputChange(int change)
+    {
+        ChangeEntered += change;
+    }
+}
