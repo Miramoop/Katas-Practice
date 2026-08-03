15ee7674d15931c20fd961c26e891f304183da86
Miramoop <94880693+Miramoop@users.noreply.github.com>
2026-08-02 22:30:41 -0400

    Add July 30 and July 31 Gilded Rose Attempts

---
+++ Attempts/GildedRose-csharp.xUnit 7-30/GildedRoseTests/GildedRoseTest.cs
@@ -0,0 +1,161 @@
+﻿using Xunit;
+using System.Collections.Generic;
+using GildedRoseKata;
+
+namespace GildedRoseTests;
+
+public class GildedRoseTest
+{
+    [Fact]
+    public void foo()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal("foo", Items[0].Name);
+    }
+    [Fact]
+    public void UpdateQuality_GivenfooQualityZero_ReturnQualityZero()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal(0, Items[0].Quality);
+    }
+    [Fact]
+    public void UpdateQuality_GivenfooQualityOne_ReturnQualityZero()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 1 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal(0, Items[0].Quality);
+    }
+    [Fact]
+    public void UpdateQuality_GivenfooQualityTwoSellInZero_ReturnQualityZero()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 2 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal(0, Items[0].Quality);
+    }
+    [Fact]
+    public void UpdateQuality_GivenfooQualityTwoSellInOne_ReturnQualityOne()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 1, Quality = 2 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal(1, Items[0].Quality);
+    }
+    [Fact]
+    public void UpdateQuality_GivenfooQualityTwoSellInNegativeOne_ReturnQualityZero()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = -1, Quality = 2 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal(0, Items[0].Quality);
+    }
+    [Fact]
+    public void UpdateQuality_GivenfooQualityFifty_ReturnQualityFortyEight()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 50 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal(48, Items[0].Quality);
+    }
+    [Fact]
+    public void UpdateQuality_GivenfooSellInZero_ReturnSellInNegativeOne()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 2 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal(-1, Items[0].SellIn);
+    }
+    [Fact]
+    public void UpdateQuality_GivenBrieQualityTwo_ReturnQualityFour()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 0, Quality = 2 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal(4, Items[0].Quality);
+    }
+    [Fact]
+    public void UpdateQuality_GivenBrieQualityFiftyFive_ReturnQualityFiftyFive()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 0, Quality = 55 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal(55, Items[0].Quality);
+    }
+    [Fact]
+    public void UpdateQuality_GivenBrieSellInOne_ReturnQualitySix()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 1, Quality = 5 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal(6, Items[0].Quality);
+    }
+    [Fact]
+    public void UpdateQuality_GivenBackstageQualityFiveSellInZero_ReturnQualityZero()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 0, Quality = 5 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal(0, Items[0].Quality);
+    }
+    [Fact]
+    public void UpdateQuality_GivenBackstageQualityFiveSellInZero_ReturnQualityEight()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 1, Quality = 5 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal(8, Items[0].Quality);
+    }
+    [Fact]
+    public void UpdateQuality_GivenBackstageQualityFiveSellInTwo_ReturnQualitySix()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 12, Quality = 5 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal(6, Items[0].Quality);
+    }
+    [Fact]
+    public void UpdateQuality_GivenBackstageQualityFiveSellInSeven_ReturnQualitySeven()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 7, Quality = 5 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal(7, Items[0].Quality);
+    }
+    [Fact]
+    public void UpdateQuality_GivenBackstageQualityFiveSellInSeven_ReturnQualitySix()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 7, Quality = 5 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal(6, Items[0].SellIn);
+    }
+    [Fact]
+    public void UpdateQuality_GivenBackstageQualityFiveSellInZero_ReturnSellInNegativeOne()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 0, Quality = 5 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal(-1, Items[0].SellIn);
+    }
+    [Fact]
+    public void UpdateQuality_GivenSulfurasQualityFive_ReturnQualityFive()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 5 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal(5, Items[0].Quality);
+    }
+    [Fact]
+    public void UpdateQuality_GivenSulfurasSellInFive_ReturnSellInFive()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 5, Quality = 5 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal(5, Items[0].SellIn);
+    }
+}
---
+++ Attempts/GildedRose-csharp.xUnit 7-31/GildedRose/GildedRose.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections.Generic;
+
+namespace GildedRoseKata;
+
+// Look into polymorphism to refactor more!!
+
+public class GildedRose
+{
+    public const string AgedBrie = "Aged Brie";
+    public const string BackstagePassesToATafkal80EtcConcert = "Backstage passes to a TAFKAL80ETC concert";
+    public const string SulfurasHandOfRagnaros = "Sulfuras, Hand of Ragnaros";
+    IList<Item> Items;
+
+    public GildedRose(IList<Item> Items)
+    {
+        this.Items = Items;
+    }
+
+    public void UpdateQuality()
+    {
+        for (var i = 0; i < Items.Count; i++)
+        {
+            if (Items[i].Name == SulfurasHandOfRagnaros)
+            {
+                continue;
+            }
+
+            switch (Items[i].Name)
+            {
+                case AgedBrie:
+                {
+                    IncreaseQualityIfPossible(i);
+                    break;
+                }
+                case BackstagePassesToATafkal80EtcConcert:
+                {
+                    UpdateBackstagePass(i);
+                    break;
+                }
+                default:
+                {
+                    if (Items[i].Quality > 0)
+                    {
+                        ReduceQualityBy1(i);
+                    }
+
+                    break;
+                }
+            }
+
+            Items[i].SellIn--;
+
+            if (Items[i].SellIn < 0)
+            {
+                if (Items[i].Name == AgedBrie)
+                {
+                    IncreaseQualityIfPossible(i);
+                }
+                else
+                {
+                    if (Items[i].Name == BackstagePassesToATafkal80EtcConcert)
+                    {
+                        Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                    }
+                    else
+                    {
+                        if (Items[i].Quality > 0)
+                        {
+                            ReduceQualityBy1(i);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    private void UpdateBackstagePass(int i)
+    {
+        IncreaseQualityIfPossible(i);
+
+        if (Items[i].SellIn < 11)
+        {
+            IncreaseQualityIfPossible(i);
+        }
+
+        if (Items[i].SellIn < 6)
+        {
+            IncreaseQualityIfPossible(i);
+        }
+
+        return;
+    }
+
+    private void IncreaseQualityIfPossible(int i)
+    {
+        if (Items[i].Quality < 50)
+        {
+            IncrementQualityBy1(i);
+        }
+    }
+
+    private int IncrementQualityBy1(int i)
+    {
+        return Items[i].Quality = Items[i].Quality + 1;
+    }
+    
+    private void ReduceQualityBy1(int i)
+    {
+        Items[i].Quality = Items[i].Quality - 1;
+    }
+}
